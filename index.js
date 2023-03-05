2d6ab5cdee622cc885f7d68659c1a55384c17336
luo-qiming <lowuji22@gmail.com>
2023-03-06 01:20:09 +0800

    good logic, but js floating point number has weir error. add specific condition to deal with it, but not sure if it will suit other scenario

--- index.js
+++ index.js
@@ -21,59 +21,59 @@ function checkCashRegister(price, cash, cid) {
     if(diffCash > 0) {
         let cidReverse = [...cid].reverse();
         
-        // build an array of cid object
+        // build an array of cidReverse object
         const cidObjs = [
             {
                 unit: 100,
-                unitNmae: 'ONE HUNDRED',
+                unitName: 'ONE HUNDRED',
                 amount: cidReverse[0][1],
                 number: cidReverse[0][1]/100
             },
             {
                 unit: 20,
-                unitNmae: 'TWENTY',
+                unitName: 'TWENTY',
                 amount: cidReverse[1][1],
                 number: cidReverse[1][1]/20
             },
             {
                 unit: 10,
-                unitNmae: 'TEN',
+                unitName: 'TEN',
                 amount: cidReverse[2][1],
                 number: cidReverse[2][1]/10
             },
             {
                 unit: 5,
-                unitNmae: 'FIVE',
+                unitName: 'FIVE',
                 amount: cidReverse[3][1],
                 number: cidReverse[3][1]/5
             },
             {
                 unit: 1,
-                unitNmae: 'ONE',
+                unitName: 'ONE',
                 amount: cidReverse[4][1],
                 number: cidReverse[4][1]/1
             },
             {
                 unit: 0.25,
-                unitNmae: 'QUARTER',
+                unitName: 'QUARTER',
                 amount: cidReverse[5][1],
                 number: cidReverse[5][1]/0.25
             },
             {
                 unit: 0.1,
-                unitNmae: 'DIME',
+                unitName: 'DIME',
                 amount: cidReverse[6][1],
                 number: cidReverse[6][1]/0.1
             },
             {
                 unit: 0.05,
-                unitNmae: 'NICKEL',
+                unitName: 'NICKEL',
                 amount: cidReverse[7][1],
                 number: cidReverse[7][1]/0.05
             },
             {
                 unit: 0.01,
-                unitNmae: 'PENNY',
+                unitName: 'PENNY',
                 amount: cidReverse[8][1],
                 number: cidReverse[8][1]/0.01
             },
@@ -83,6 +83,8 @@ function checkCashRegister(price, cash, cid) {
         //
         let dynamicChange = change;
         let realChange = [];
+
+        /*
         for(let i=0; i<cidObjs.length; i++){
             let x = Math.floor(dynamicChange/cidObjs[i].unit);
             if(x > 0){
@@ -95,12 +97,33 @@ function checkCashRegister(price, cash, cid) {
                 }
             }
         }
-        
-        if(dynamicChange > 0){
+        */
+
+        for(let i = 0; i<cidObjs.length; i++){
+            if(dynamicChange >= cidObjs[i].unit && cidObjs[i].amount>0){
+                if(dynamicChange >= cidObjs[i].amount){
+                    realChange.push([cidObjs[i].unitName, cidObjs[i].amount]); // take all
+                    dynamicChange -= cidObjs[i].amount;
+                }else{
+                    let amountTaking = 0;
+                    while(dynamicChange >= cidObjs[i].unit){  // if dynamicChange < amount, take n*unit unless dynamicChange < unit
+                        amountTaking += cidObjs[i].unit;
+                        dynamicChange = dynamicChange - cidObjs[i].unit;
+                    }
+                    realChange.push([cidObjs[i].unitName, amountTaking]);
+                }
+            }
+        }  // at the end, dynamicChange should be 0 or dynamicChange > 0.
+
+        if(dynamicChange >= 0.01){
             console.log('{status: "INSUFFICIENT_FUNDS", change: []}');
+        }else if(dynamicChange >0 && dynamicChange < 0.01){  // to deal with floating point number error
+            realChange[realChange.length-1][1] += 0.01;
+            console.log(realChange);
         }else{
             console.log(realChange);
         }
+        
 
     }
     
@@ -108,3 +131,5 @@ function checkCashRegister(price, cash, cid) {
 
 // test
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
