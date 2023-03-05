ba61da681cc79bd7221db0d5b86543160f17a72a
luoqiming82 <lowuji22@gmail.com>
2023-03-05 18:01:17 +0800

    build array of obj, to deal with cash change

--- index.js
+++ index.js
@@ -26,49 +26,52 @@ function checkCashRegister(price, cash, cid) {
             {
                 unit: 100,
                 amount: cidReverse[0][1],
-                number: amount/unit
+                number: cidReverse[0][1]/100
             },
             {
                 unit: 20,
                 amount: cidReverse[1][1],
-                number: amount/unit
+                number: cidReverse[1][1]/20
             },
             {
                 unit: 10,
                 amount: cidReverse[2][1],
-                number: amount/unit
+                number: cidReverse[2][1]/10
             },
             {
                 unit: 5,
                 amount: cidReverse[3][1],
-                number: amount/unit
+                number: cidReverse[3][1]/5
             },
             {
                 unit: 1,
                 amount: cidReverse[4][1],
-                number: amount/unit
+                number: cidReverse[4][1]/1
             },
             {
                 unit: 0.25,
                 amount: cidReverse[5][1],
-                number: amount/unit
+                number: cidReverse[5][1]/0.25
             },
             {
                 unit: 0.1,
                 amount: cidReverse[6][1],
-                number: amount/unit
+                number: cidReverse[6][1]/0.1
             },
             {
                 unit: 0.05,
                 amount: cidReverse[7][1],
-                number: amount/unit
+                number: cidReverse[7][1]/0.05
             },
             {
                 unit: 0.01,
                 amount: cidReverse[8][1],
-                number: amount/unit
+                number: cidReverse[8][1]/0.01
             },
         ]
+
+        console.log(cidObj[8]);
+        //
 
     }
     
